7381d3d578d4876e8d618bdeb994696231bafcd3
maiatoday <maiatoday@gmail.com>
2015-03-24 10:00:38 +0200

    clean up comms, reorganise in files

--- arduino/interrupt/interrupt.ino
+++ arduino/interrupt/interrupt.ino
@@ -30,6 +30,9 @@
 
 #define mybaud           9600
 
+byte  myID;
+byte  toID;
+
 long lastDebounceTime = 0;  // the last time the output pin was toggled
 long debounceDelay = 200;    // the debounce time; increase if the output flickers
 
@@ -38,18 +41,7 @@ long displayDelay = 300;
 
 volatile boolean stateChange;
 
-byte  data[13];
-byte  address;
-byte  myID;
-byte  toID;
-unsigned int data_received;
-byte  byte_receive;
-byte  state=0;
-byte  cont1=1;
-byte  trace_OK=0;
-unsigned int checksum;
-unsigned int checksum_trace;
-
+//************ display variables
 enum stateDisplayType {
   notHR,
   HR
@@ -59,26 +51,26 @@ stateDisplayType stateDisplay;
 
 void setup() {
   //setup Input pin and Interrupt
-  pinMode(PinINPUT0,INPUT_PULLUP);
+  pinMode(PinINPUT0, INPUT_PULLUP);
   PCintPort::attachInterrupt(PinINPUT0, pulseISR, CHANGE); // attach a PinChange Interrupt to our pin CHANGE means any level change triggers an interrupt
 
   stateChange = false;
   stateDisplay = notHR;
 
-  pinMode(PinLED,OUTPUT);
-  pinMode(RS485Control,OUTPUT);
-  digitalWrite(RS485Control,RS485Receive);
+  pinMode(PinLED, OUTPUT);
+  pinMode(RS485Control, OUTPUT);
+  digitalWrite(RS485Control, RS485Receive);
 
   Serial.begin(mybaud);
 
-  pinMode(ledB,OUTPUT);
-  pinMode(ledR,OUTPUT);
-  pinMode(ledG,OUTPUT);
+  pinMode(ledB, OUTPUT);
+  pinMode(ledR, OUTPUT);
+  pinMode(ledG, OUTPUT);
 
-  pinMode(PinADDR0,INPUT_PULLUP);
-  pinMode(PinADDR1,INPUT_PULLUP);
-  pinMode(PinADDR2,INPUT_PULLUP);
-  pinMode(PinADDR3,INPUT_PULLUP);
+  pinMode(PinADDR0, INPUT_PULLUP);
+  pinMode(PinADDR1, INPUT_PULLUP);
+  pinMode(PinADDR2, INPUT_PULLUP);
+  pinMode(PinADDR3, INPUT_PULLUP);
 
   myID = 0;
   myID = myID + !digitalRead(PinADDR0);
@@ -100,7 +92,7 @@ void checkSend() {
   if (stateChange) {
     debugCommsTx(true);
     stateChange = false;
-    sendMSG(48,toID+48,'P');
+    sendMSG(myID, toID, 'P');
     debugCommsTx(false);
   }
 }
@@ -112,133 +104,33 @@ void pulseISR() {
 //--------------- Visual Methods
 void changeVisuals() {
   //Check if HR received in last 4 beats
-  if ((millis() - lastMessageTime) > 4*debounceDelay) {
+  if ((millis() - lastMessageTime) > 4 * debounceDelay) {
     stateDisplay = notHR;
   } else {
     debugVisuals(true);
     stateDisplay = HR;
-  /* mg
-  //display state machine
-  //in hr
-  if (stateDisplay == HR)
-    if (stateChange){
-      //digitalWrite(PinLED,HIGH);
-      setPixelColor(255,255,255);
-       = millis();
-    } else {
-      if ((millis() - ) > displayDelay) {
-        //digitalWrite(PinLED,LOW);
-        setPixelColor(0,0,0);
+    /* mg
+    //display state machine
+    //in hr
+    if (stateDisplay == HR)
+      if (stateChange){
+        //digitalWrite(PinLED,HIGH);
+        setPixelColor(255,255,255);
+         = millis();
+      } else {
+        if ((millis() - ) > displayDelay) {
+          //digitalWrite(PinLED,LOW);
+          setPixelColor(0,0,0);
+        }
       }
-    }
-  else
-    //digitalWrite(PinLED,LOW);
-    setPixelColor(0,0,0);
-  //in not hr
-  */
+    else
+      //digitalWrite(PinLED,LOW);
+      setPixelColor(0,0,0);
+    //in not hr
+    */
     debugVisuals(false);
   }
 }
-
-void setPixelColor(byte r, byte g, byte b)
-{
-  analogWrite(ledR,r);
-  analogWrite(ledG,g);
-  analogWrite(ledB,b);
-}
-
-//---------------- Comms Rx methods
-void readLoop() {
-  while (Serial.available() > 0) {
-  
-   // debugCommsRx(true);
-    byte_receive=Serial.read();
-    
-    if (byte_receive==00) {
-      state=1;
-      checksum_trace=0;
-      checksum=0;
-      trace_OK=0;
-      address=0;
-      data_received=0;
-      cont1=1;
-    } else if (state==1 && cont1<=4) {
-      data[cont1-1]=byte_receive; // evil line
-      checksum=checksum+byte_receive;
-      cont1=cont1+1;
-//   Needed if data was longer
-//    } else if (state==1 && cont1==4) {
-//      checksum_trace=byte_receive<<8;
-//      cont1=cont1+1;
-    } else if (state==1 && cont1==5) {
-      checksum_trace=checksum_trace+byte_receive;
-      cont1=cont1+1;
-      state=0;
-      
-      if (checksum_trace==checksum) {
-        trace_OK=1;
-        if (data[0] == 49) {
-          address=10+hex2num(data[1]);
-        } else {
-          address=hex2num(data[1]);
-        }
-        if (address==myID) {
-          if (data[2] == 'P') {
-            stateDisplay = HR;
-            lastMessageTime = millis();
-            //sendMSG(49,toID+48,'D');
-
-          } else {
-          //sendMSG(48,toID+48,'M');
-          delay(1);
-          }
-        } else {
-          //sendMSG(48,toID+48,'N');
-          delay(1);
-        }
-      }
-      
-    } //else if state 1 count 5
-         
-    debugCommsRx(false);
-  } // while
-}
-
-//--------------- Comms TX methods
-void sendMSG(byte address1,byte address2,byte data){
-  sendData(tENQ,address1,address2,data);
-}
-
-void sendACK(byte address1,byte address2,byte data){
-  sendData(tACK,address1,address2,data);
-}
-
-void sendNAK(byte address1,byte address2,byte data){
-  sendData(tNAK,address1,address2,data);
-}
-
-void sendData(byte type, byte address1,byte address2,byte data){
-
-  unsigned int checksum_ACK;
-  checksum_ACK=address1+address2+data+3;
-
-  digitalWrite(RS485Control, RS485Transmit);  // Enable RS485 Transmit
-
-  delay(1);
-
-  Serial.write(0);
-  Serial.write(address1);
-  Serial.write(address2);
-  Serial.write(data);
-  Serial.write(3);
-//  Needed if data was longer
-//  Serial.write(((checksum_ACK>>8)&255));
-  Serial.write(((checksum_ACK)&255));
-  Serial.flush();
-
-  digitalWrite(RS485Control, RS485Receive);  // Disable RS485 Transmit
-}
-
 
 //-------------- Debug methods
 void debugPulse(boolean on) {
@@ -246,13 +138,13 @@ void debugPulse(boolean on) {
 }
 
 void debugCommsTx(boolean on) {
-    digitalWrite(PinLED,on?HIGH:LOW);
+  digitalWrite(PinLED, on ? HIGH : LOW);
 }
 
 void debugCommsRx(boolean on) {
-    //digitalWrite(PinLED,on?HIGH:LOW);
+  //digitalWrite(PinLED,on?HIGH:LOW);
 }
 
 void debugVisuals(boolean on) {
-//    digitalWrite(PinLED,on?HIGH:LOW);
+  //    digitalWrite(PinLED,on?HIGH:LOW);
 }
